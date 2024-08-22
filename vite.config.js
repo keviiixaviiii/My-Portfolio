0557e354e09c1925272adf1e9f4c6a610684e7b4
iamkevinxavi <iamkevinxavi@gmail.com>
2024-08-22 15:08:11 +0000

    updated vit.config

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/My-Portfolio/',  // Add this line to set the base URL for GitHub Pages
   css: {
     modules: {
       localsConvention: "camelCase",
