7cef6dd9051fa44c4d5e25bdce8a2ef664fbdee2
kevin xavier <62089808+keviiixaviiii@users.noreply.github.com>
2024-08-26 15:08:16 +0100

    Update vite.config.js
    updated for netifly deplyments

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/My-Portfolio/',  // Add this line to set the base URL for GitHub Pages
+  base: '/',  // Set to '/' for Netlify, or remove entirely
   css: {
     modules: {
       localsConvention: "camelCase",
